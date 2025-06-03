c8970c75b9547496c87f787ed1f319e6501e1d1d
bdvzt <budaevazay@mail.ru>
2025-06-03 14:11:10 +0700

    Создала модуль букинг-сервис ДАЙТЕ СИЛ РАЗОБРАТЬСЯ С КАФКОЙ

--- booking-service/src/main/java/com/example/bookingservice/BookingServiceApplication.java
+++ booking-service/src/main/java/com/example/bookingservice/BookingServiceApplication.java
@@ -1,13 +1,13 @@
-package com.example.car_rental;
+package com.example.bookingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CarRentalApplication {
+public class BookingServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CarRentalApplication.class, args);
+		SpringApplication.run(BookingServiceApplication.class, args);
 	}
 
 }
